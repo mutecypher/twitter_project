9b902f1345a4e50dba9d1f253acb1dec31adb7a3
mutecypher <mutecypher@icloud.com>
2022-07-30 13:36:26 -0700

    Update AP2_scrape.py

--- AP2_scrape.py
+++ AP2_scrape.py
@@ -15,7 +15,7 @@ API_key = 'OEnnzk5ro76PdhQHTJVxa3qTv'
 # API_key = 'qchdahMH5QJp0xd1dPQPTdxAJ'
 API_SECRET_KEY = 'sj4lfiejDGosykNA62YaHqBP1J9RyESbMqKboxQp9sW51gJylC'
 
-how_many_tries = 36
+how_many_tries = 72
 items_to_get = 100
 whats_today = datetime.datetime.now()
 # whats_today = whats_today.strftime("%Y%m%d%H%M")
@@ -39,7 +39,7 @@ def get_them_tweets(src_wd_1, src_wd_2, num_hrs, num_tweets):
 
     tweetz = []
     for i in range(num_hrs):
-        time.sleep(2.0)
+        time.sleep(1.0)
         strt_date = datetime.datetime.now() - datetime.timedelta(hours=i)
         stp_date = datetime.datetime.now() - datetime.timedelta(hours=i+1)
 
@@ -86,91 +86,46 @@ def get_them_tweets(src_wd_1, src_wd_2, num_hrs, num_tweets):
     return tweetz
 
 
-headley = ['index', 'created_at', 'user_id', 'text', 'source',
-           'lang', 'likes', 'retweets']
+def filing_it_away(get_the_file, d_frame, stock):
+    file_name = '/Volumes/Elements/GitHub/twitter-project/Data_Files/' + get_the_file
+    ricky_bobby = pd.read_table(file_name, sep=',', header='infer')
+    print("the initial shape is ", ricky_bobby.shape)
+    ricky_bobby.append(d_frame)
+    print("the appended shape is ", ricky_bobby.shape)
+##
+    ricky_bobby = ricky_bobby[ricky_bobby['lang'] == 'en']
+    ricky_bobby = ricky_bobby.drop_duplicates()
+    ricky_bobby.reset_index(inplace=True, drop=True)
+    print("the final shape is ", ricky_bobby.shape)
+
+    ricky_bobby.to_csv(file_name, index=False)
+    hasta = "Saul Goodman" + " " + stock
+    print(hasta)
+
+
 tweets_AMZN = get_them_tweets('AMZN', 'Amazon', how_many_tries, items_to_get)
 
 Amazon_df = pd.DataFrame(tweets_AMZN)
 
-
-print(Amazon_df.head())
-
-
-Amazon_df.to_csv(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/Amazon_df_api2.csv', mode='a', index=True, header=False)
-
-print("the AMZN_rob shape is", Amazon_df.shape)
-
-# ricky_bobby = pd.read_csv(
-# '/Volumes/Elements/GitHub/twitter-project/Data Files/Amazon_df_api2.csv', header='infer')
-
-ricky_bobby = pd.read_table(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/Amazon_df_api2.csv', sep=',', header='infer')
-print(ricky_bobby.head())
-print("the initial shape is ", ricky_bobby.shape)
-##
-ricky_bobby = ricky_bobby[ricky_bobby['lang'] == 'en']
-ricky_bobby = ricky_bobby.drop_duplicates()
-ricky_bobby.reset_index(inplace=True, drop=True)
+filing_it_away('Amazon_df_api2.csv', Amazon_df, "Amazon")
 
 
-print("the final shape is ", ricky_bobby.shape)
-
-ricky_bobby.to_csv(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/Amazon_df_api2.csv', index=False)
-print("saulgoodman Amazon")
 #################
 
 tweets_ATEN = get_them_tweets('ATEN', 'A10', how_many_tries, items_to_get)
 
 ATEN_df = pd.DataFrame(tweets_ATEN)
 
-headley2 = ['created_at', 'user_id', 'text', 'source',
-            'lang', 'likes', 'retweets']
-ATEN_df.to_csv(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/ATEN_df_api2.csv', mode='a', index=True, header=False)
 
-ricky_bobby = pd.read_table(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/ATEN_df_api2.csv', sep=',', header='infer')
-print(ricky_bobby.head())
-print("the initial shape is ", ricky_bobby.shape)
-##
-ricky_bobby = ricky_bobby[ricky_bobby['lang'] == 'en']
-ricky_bobby = ricky_bobby.drop_duplicates()
-ricky_bobby.reset_index(inplace=True, drop=True)
+filing_it_away('ATEN_df_api2.csv', ATEN_df, "ATEN")
 
-
-print("the final shape is ", ricky_bobby.shape)
-
-ricky_bobby.to_csv(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/ATEN_df_api2.csv', index=False)
-print("saulgoodman ATEN")
 
 #################
-tweets_ATEN = get_them_tweets('ATEN', 'A10', how_many_tries, items_to_get)
-print("OK jot")
+
 tweets_KMI = get_them_tweets(
     'KMI', 'Kinder', how_many_tries, items_to_get)
 
 KMI_df = pd.DataFrame(tweets_KMI)
 
-headley2 = ['created_at', 'user_id', 'text', 'source',
-            'lang', 'likes', 'retweets']
-KMI_df.to_csv(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/KMI_df_api2.csv', mode='a', index=True, header=False)
 
-ricky_bobby = pd.read_table(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/KMI_df_api2.csv', sep=',', header='infer')
-print(ricky_bobby.head())
-print("the initial shape is ", ricky_bobby.shape)
-##
-ricky_bobby = ricky_bobby[ricky_bobby['lang'] == 'en']
-ricky_bobby = ricky_bobby.drop_duplicates()
-ricky_bobby.reset_index(inplace=True, drop=True)
-
-
-print("the final shape is ", ricky_bobby.shape)
-
-ricky_bobby.to_csv(
-    '/Volumes/Elements/GitHub/twitter-project/Data_Files/KMI_df_api2.csv', index=False)
-print("saulgoodman KMI")
+filing_it_away('KMI_df_api2.csv', KMI_df, "KMI")
