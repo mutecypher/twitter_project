9c951d81144a053caa95c12d3c1c090fc7b47da2
mutecypher <mutecypher@icloud.com>
2023-05-11 12:22:48 -0700

    just stuff

--- Yahoo_scrape.py
+++ Yahoo_scrape.py
@@ -9,7 +9,7 @@ whats_today = datetime.datetime.now().date()
 
 
 # May 2023 1132
-minus_fifteen_years = whats_today - datetime.timedelta(days=1132 + 8)
+minus_fifteen_years = whats_today - datetime.timedelta(days=1132 + 9)
 
 
 whats_today = whats_today.strftime('%Y-%m-%d')
